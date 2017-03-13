110a05d2884205f3cadca41c9eda81264847e197
Bastien <Bastien@172.29.6.224>
2017-03-13 10:21:33 +0100

    bdd

--- Forum/src/servlets/Connexion.java
+++ Forum/src/servlets/Connexion.java
@@ -1,8 +1,9 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -39,7 +40,66 @@ public class Connexion extends HttpServlet {
 
 	        String email = request.getParameter( CHAMP_EMAIL );
 
-	        String motDePasse = request.getParameter( CHAMP_PASS );
+	        String Passe = request.getParameter( CHAMP_PASS );
+	        
+	        
+
+	        try {
+
+	            Class.forName( "com.mysql.jdbc.Driver" );
+
+	        } catch ( ClassNotFoundException e ) {
+
+	            /* Gérer les éventuelles erreurs ici. */
+
+	        }
+	        
+	        /* Connexion à la base de données */
+
+	        String url = "jdbc:mysql://localhost:3306/forum";
+
+	        String utilisateur = "root";
+
+	        String motDePasse = "";
+
+	        Connection connexion = null;
+
+	        
+	        
+	        try {
+
+	            connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+
+
+	            /* Ici, nous placerons nos requêtes vers la BDD */
+
+	            /* ... */
+
+
+	        } catch ( SQLException e ) {
+
+	            /* Gérer les éventuelles erreurs ici */
+
+	        } finally {
+
+	            if ( connexion != null )
+
+	                try {
+
+	                    /* Fermeture de la connexion */
+
+	                    connexion.close();
+
+	                } catch ( SQLException ignore ) {
+
+	                    /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+
+	                }
+
+	        }
+	        
+	        
+	        
 	        
 	        
 		}
